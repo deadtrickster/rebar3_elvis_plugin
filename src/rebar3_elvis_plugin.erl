2e0929d0ec559ef8a61968d8dbc7a87b0c32f663
Ilya Khaprov <dead.trickster@gmail.com>
2016-12-24 15:49:55 +0300

    plugin

---
+++ src/rebar3_elvis_plugin.erl
@@ -0,0 +1,55 @@
+-module(rebar3_elvis_plugin).
+
+-export([init/1,
+         do/1,
+         format_error/1]).
+
+-define(PROVIDER, elvis).
+-define(DEPS, [compile]).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+  Provider = providers:create([{name, ?PROVIDER},
+                               {module, ?MODULE},
+                               {deps, ?DEPS},
+                               {bare, true},
+                               {example, "rebar3 elvis"},
+                               {short_desc, short_desc()},
+                               {desc, desc()}]),
+  State1 = rebar_state:add_provider(State, Provider),
+  {ok, State1}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+  Config = rebar_state:get(State, elvis, elvis_config:default()),
+  case elvis_core:rock(Config) of
+    ok ->
+      {ok, State};
+    {fail, Files} ->
+      {error, {?MODULE, Files}}
+  end.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+  io_lib:format("~p", [Reason]).
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+short_desc() ->
+  "Run Elvis, the Erlang style reviewer.".
+
+desc() ->
+  io_lib:format(
+    "~s~n"
+    "~n"
+    "Valid elvis options:~n"
+    "  ~p~n",
+    [short_desc(),
+     "https://github.com/inaka/elvis_core/wiki/Rules"
+    ]).

eccc139e8954778b01d0efe4d6e1ce593488c958
Ilya Khaprov <dead.trickster@gmail.com>
2016-12-28 15:57:52 +0300

    bump to 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rebar3ElvisPlugin.Mixfile do
 
   def project do
     [app: :rebar3_elvis_plugin,
-     version: "0.0.3",
+     version: "0.0.4",
      description: description,
      deps: deps(),
      package: package]
